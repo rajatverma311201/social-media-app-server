3a5395455fbe6d7ba807804f134d7b03ab18fe71
Rajat Verma <rajat311201@gmail.com>
2023-05-30 13:57:11 +0530

    updated routes

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const followController = require("../controllers/followController");
-const authController = require("../controllers/authController");
 
 const router = express.Router();
 
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const authController = require("../controllers/authController");
 const postController = require("../controllers/postController");
 
 router
